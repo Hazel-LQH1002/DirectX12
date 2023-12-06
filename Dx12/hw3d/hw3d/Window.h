4d0d5faa152355cc5775df2c940d966d67c16089
Hazel Liang <81887643+Hazel-LQH1002@users.noreply.github.com>
2023-12-06 15:26:22 +0800

    put macros together
    put macro to macro header file

--- Dx12/hw3d/hw3d/Window.h
+++ Dx12/hw3d/hw3d/Window.h
@@ -79,7 +79,3 @@ private:
 	HWND hWnd;
 	std::unique_ptr<Graphics> pGfx;//defer the construction of graphics
 };
-
-#define CHWND_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,(hr) )
-#define CHWND_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
-#define CHWND_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
