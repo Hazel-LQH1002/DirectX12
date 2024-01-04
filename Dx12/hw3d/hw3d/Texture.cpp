d775634f21afcfa31a970f396847b8cffa8af6b3
Hazel Liang <81887643+Hazel-LQH1002@users.noreply.github.com>
2024-01-04 22:21:18 +0000

    add imgui

--- Dx12/hw3d/hw3d/Texture.cpp
+++ Dx12/hw3d/hw3d/Texture.cpp
@@ -12,8 +12,8 @@ Texture::Texture(Graphics& gfx, const Surface& s)
 	D3D11_TEXTURE2D_DESC textureDesc = {};
 	textureDesc.Width = s.GetWidth();
 	textureDesc.Height = s.GetHeight();
-	textureDesc.MipLevels = 1;
-	textureDesc.ArraySize = 1;
+	textureDesc.MipLevels = 1;//not using mipmapping
+	textureDesc.ArraySize = 1;//just create one texture
 	textureDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 	textureDesc.SampleDesc.Count = 1;
 	textureDesc.SampleDesc.Quality = 0;
@@ -23,7 +23,7 @@ Texture::Texture(Graphics& gfx, const Surface& s)
 	textureDesc.MiscFlags = 0;
 	D3D11_SUBRESOURCE_DATA sd = {};
 	sd.pSysMem = s.GetBufferPtr();
-	sd.SysMemPitch = s.GetWidth() * sizeof(Surface::Color);
+	sd.SysMemPitch = s.GetWidth() * sizeof(Surface::Color);//distance of bites between pixels
 	wrl::ComPtr<ID3D11Texture2D> pTexture;
 	GFX_THROW_INFO(GetDevice(gfx)->CreateTexture2D(
 		&textureDesc, &sd, &pTexture
@@ -33,14 +33,15 @@ Texture::Texture(Graphics& gfx, const Surface& s)
 	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 	srvDesc.Format = textureDesc.Format;
 	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MostDetailedMip = 0;
+	srvDesc.Texture2D.MostDetailedMip = 0;//not using mipmapping
 	srvDesc.Texture2D.MipLevels = 1;
 	GFX_THROW_INFO(GetDevice(gfx)->CreateShaderResourceView(
-		pTexture.Get(), &srvDesc, &pTextureView
+		pTexture.Get(), &srvDesc, &pTextureView  //--> to fill the view with the created texture
+		//pointer to texture; descriptor; pp to fill view
 	));
 }
 
 void Texture::Bind(Graphics& gfx) noexcept
 {
-	GetContext(gfx)->PSSetShaderResources(0u, 1u, pTextureView.GetAddressOf());
+	GetContext(gfx)->PSSetShaderResources(0u, 1u, pTextureView.GetAddressOf());//set the texture view to pixel shader
 }
